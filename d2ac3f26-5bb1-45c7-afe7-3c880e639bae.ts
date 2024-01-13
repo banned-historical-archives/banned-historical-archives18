5b5dcb15f1a369e9e916279613f86fe7f799aaee
Your Name <you@example.com>
2024-01-13 14:03:17 +0000

    feat: [自动化录入]文革通讯第34期

---
+++ d2ac3f26-5bb1-45c7-afe7-3c880e639bae.ts
@@ -0,0 +1,272 @@
+export default {
+  resource_type: 'book',
+  entity: {
+    id: 'd2ac3f26-5bb1-45c7-afe7-3c880e639bae',
+    name: '文革通讯第34期',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '浙江工农兵美术大学红卫兵战斗队、毛泽东主义红卫兵',
+    files: ["https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives18/main/d2ac3f26-5bb1-45c7-afe7-3c880e639bae.pdf"]
+  },
+  parser_option: {
+    articles: [
+  {
+    "title": "毛主席的三点指示",
+    "authors": [
+      "毛泽东"
+    ],
+    "page_start": 1,
+    "page_end": 1,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 19
+      }
+    ]
+  },
+  {
+    "title": "主席永远是我们学习的最高典范——主席生活记实",
+    "authors": [
+      "文革通讯编辑部"
+    ],
+    "page_start": 1,
+    "page_end": 2,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 19
+      }
+    ]
+  },
+  {
+    "title": "周总理、中央文革小组的讲话",
+    "authors": [
+      "周恩来",
+      "江青",
+      "陈伯达"
+    ],
+    "page_start": 2,
+    "page_end": 3,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 22
+      }
+    ]
+  },
+  {
+    "title": "4月21日中央首长谈联动",
+    "authors": [
+      "陈伯达",
+      "江青"
+    ],
+    "page_start": 3,
+    "page_end": 3,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 21
+      }
+    ]
+  },
+  {
+    "title": "周恩来在广州革命群众组织代表及驻军代表座谈会上的讲话",
+    "authors": [
+      "周恩来"
+    ],
+    "page_start": 3,
+    "page_end": 3,
+    "dates": [
+      {
+        "year": 1967
+      }
+    ]
+  },
+  {
+    "title": "首长接见“联动”：“主席给你改正的机会”",
+    "authors": [
+      "周恩来"
+    ],
+    "page_start": 3,
+    "page_end": 4,
+    "dates": [
+      {
+        "year": 1967
+      }
+    ]
+  },
+  {
+    "title": "中央文革关于释放“联动”小头目的重要指示",
+    "authors": [
+      "江青",
+      "戚本禹",
+      "姚文元"
+    ],
+    "page_start": 4,
+    "page_end": 5,
+    "is_range_date": true,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 22
+      },
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 23
+      }
+    ]
+  },
+  {
+    "title": "江青、总理、伯达谈“联动”",
+    "authors": [
+      "江青",
+      "周恩来",
+      "陈伯达"
+    ],
+    "page_start": 5,
+    "page_end": 5,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 22
+      }
+    ]
+  },
+  {
+    "title": "周总理四月三十日接见中央机关各派代表的讲话",
+    "authors": [
+      "周恩来"
+    ],
+    "page_start": 6,
+    "page_end": 6,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 30
+      }
+    ]
+  },
+  {
+    "title": "中央首长对内蒙问题的指示",
+    "authors": [
+      "中央首长"
+    ],
+    "page_start": 6,
+    "page_end": 6,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 27
+      }
+    ]
+  },
+  {
+    "title": "关于福建问题",
+    "authors": [
+      "陈伯达",
+      "周恩来"
+    ],
+    "page_start": 6,
+    "page_end": 7,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 23
+      }
+    ]
+  },
+  {
+    "title": "陈伯达、康生、江青同志指示",
+    "authors": [
+      "陈伯达",
+      "康生",
+      "江青"
+    ],
+    "page_start": 7,
+    "page_end": 7,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 15
+      }
+    ]
+  },
+  {
+    "title": "李富春同志谈“三结合”",
+    "authors": [
+      "李富春"
+    ],
+    "page_start": 7,
+    "page_end": 7,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 22
+      }
+    ]
+  },
+  {
+    "title": "要大反特反无政府主义，谢富治4月18日接见工代会讲话",
+    "authors": [
+      "谢富治"
+    ],
+    "page_start": 8,
+    "page_end": 8,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 18
+      }
+    ]
+  },
+  {
+    "title": "在4月21日接见715革命造反团等革命组织代表时肖华、杨成武同志的讲话",
+    "authors": [
+      "肖华",
+      "杨成武"
+    ],
+    "page_start": 8,
+    "page_end": 8,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 21
+      }
+    ]
+  },
+  {
+    "title": "北影拍摄了哪些毒草影片",
+    "authors": [
+      "新北影编辑部"
+    ],
+    "page_start": 8,
+    "page_end": 8,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5
+      }
+    ]
+  }
+],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation',
+  path: 'd2ac3f26-5bb1-45c7-afe7-3c880e639bae.pdf'
+};
