a70233c287d3747f0e15aa5f3578efd357e18c69
Your Name <you@example.com>
2024-01-13 13:31:54 +0000

    feat: [自动化录入]文革通讯第33期

---
+++ 825d007a-9049-4128-8c8c-a016b5ebf25c.ts
@@ -0,0 +1,202 @@
+export default {
+  resource_type: 'book',
+  entity: {
+    id: '825d007a-9049-4128-8c8c-a016b5ebf25c',
+    name: '文革通讯第33期',
+    internal: false,
+    official: false,
+    type: 'pdf',
+    author: '浙江工农兵美术大学红卫兵战斗队、毛泽东主义红卫兵',
+    files: ["https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives18/main/825d007a-9049-4128-8c8c-a016b5ebf25c.pdf"]
+  },
+  parser_option: {
+    articles: [
+  {
+    "title": "毛主席最新指示",
+    "authors": [
+      "毛泽东"
+    ],
+    "page_start": 1,
+    "page_end": 1,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 19
+      }
+    ]
+  },
+  {
+    "title": "林副主席最新指示",
+    "authors": [
+      "林彪"
+    ],
+    "page_start": 1,
+    "page_end": 1,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 5
+      }
+    ]
+  },
+  {
+    "title": "总理在军事科学院的讲话",
+    "authors": [
+      "周恩来"
+    ],
+    "page_start": 1,
+    "page_end": 1,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 8
+      }
+    ]
+  },
+  {
+    "title": "陈伯达同志给北京市革委会的六点指示",
+    "authors": [
+      "陈伯达"
+    ],
+    "page_start": 1,
+    "page_end": 2,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5
+      }
+    ]
+  },
+  {
+    "title": "伯达、康生、江青同志的讲话",
+    "authors": [
+      "陈伯达",
+      "康生",
+      "江青"
+    ],
+    "page_start": 2,
+    "page_end": 2,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 4
+      }
+    ]
+  },
+  {
+    "title": "谢副总理接见高检院同志时的讲话",
+    "authors": [
+      "谢富治"
+    ],
+    "page_start": 2,
+    "page_end": 2,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5
+      }
+    ]
+  },
+  {
+    "title": "肖华、杨成武、关锋等同志接见军事院校“斗罗筹备处”中心组同志座谈纪要",
+    "authors": [
+      "肖华",
+      "杨成武",
+      "关锋"
+    ],
+    "page_start": 2,
+    "page_end": 2,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 8
+      }
+    ]
+  },
+  {
+    "title": "康生同志谈斗争艺术",
+    "authors": [
+      "康生"
+    ],
+    "page_start": 3,
+    "page_end": 3,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 4,
+        "day": 28
+      }
+    ]
+  },
+  {
+    "title": "戚本禹同志讲话",
+    "authors": [
+      "戚本禹"
+    ],
+    "page_start": 3,
+    "page_end": 3,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 9
+      }
+    ]
+  },
+  {
+    "title": "北京消息",
+    "authors": [
+      "文革通讯编辑部"
+    ],
+    "page_start": 3,
+    "page_end": 5,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 19
+      }
+    ]
+  },
+  {
+    "title": "祖国各地",
+    "authors": [
+      "文革通讯编辑部"
+    ],
+    "page_start": 6,
+    "page_end": 8,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 19
+      }
+    ]
+  },
+  {
+    "title": "国际动态",
+    "authors": [
+      "文革通讯编辑部"
+    ],
+    "page_start": 8,
+    "page_end": 8,
+    "dates": [
+      {
+        "year": 1967,
+        "month": 5,
+        "day": 19
+      }
+    ]
+  }
+],
+    ocr: undefined,
+    ocr_exceptions: {},
+  },
+  parser_id: 'automation'
+  path: '825d007a-9049-4128-8c8c-a016b5ebf25c.pdf'
+};
