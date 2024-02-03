46b60a9f75d8ac10e0e0dd5410b2d89aebd898cf
PirateBook1 <news_porter@protonmail.com>
2024-02-03 22:42:05 +0800

    翻身不忘毛主席——纪念毛主席诞辰121周年（2014.12.21）

---
+++ 1.ts
@@ -0,0 +1,16 @@
+export default {
+    "entity": {
+      "id": "1",
+      "name": "翻身不忘毛主席——纪念毛主席诞辰121周年（2014.12.21）",
+      "internal": false,
+      "official": false,
+      "type": "db",
+      "author": "网络资料",
+      "files": ""
+    },
+    "parser_option": {},
+    "parser_id": "result-json",
+    "path": "1",
+    "resource_type": "book",
+    "version": 2
+}
