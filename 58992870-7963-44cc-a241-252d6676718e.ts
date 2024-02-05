8bf0e862486539973b4c7f4632c108e83ddc8f61
PirateBook1 <news_porter@protonmail.com>
2024-02-05 22:12:36 +0800

    add 2

---
+++ 58992870-7963-44cc-a241-252d6676718e.ts
@@ -0,0 +1,16 @@
+export default {
+    "entity": {
+      "id": "58992870-7963-44cc-a241-252d6676718e",
+      "name": "重温“炮打司令部”（2008.3.1）",
+      "internal": false,
+      "official": false,
+      "type": "db",
+      "author": "网络资料",
+      "files": ""
+    },
+    "parser_option": {},
+    "parser_id": "result-json",
+    "path": "58992870-7963-44cc-a241-252d6676718e",
+    "resource_type": "book",
+    "version": 2
+}
---
+++ 69ee0fdf-b72d-45a7-8ec6-7b7b2120b605.ts
@@ -0,0 +1,16 @@
+export default {
+    "entity": {
+      "id": "69ee0fdf-b72d-45a7-8ec6-7b7b2120b605",
+      "name": "沉痛悼念戚本禹同志（2016.4.25）",
+      "internal": false,
+      "official": false,
+      "type": "db",
+      "author": "网络资料",
+      "files": ""
+    },
+    "parser_option": {},
+    "parser_id": "result-json",
+    "path": "69ee0fdf-b72d-45a7-8ec6-7b7b2120b605",
+    "resource_type": "book",
+    "version": 2
+}
