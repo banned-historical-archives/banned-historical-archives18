73b07d1b34325453ecd2429c3b7a46aa8d4c1282
Your Name <you@example.com>
2024-08-09 23:09:42 +0800

    1

---
+++ ec44fc53-b57a-45b4-94dc-64103cdefca8.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "ec44fc53-b57a-45b4-94dc-64103cdefca8",
+    "name": "毛泽东的金棍子 姚文元与「文革」（上）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "约翰·西西弗斯",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives18/main/ec44fc53-b57a-45b4-94dc-64103cdefca8.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "毛泽东的金棍子 姚文元与「文革」（上）",
+        "authors": [
+          "约翰·西西弗斯"
+        ],
+        "page_start": 1,
+        "page_end": 364,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "ec44fc53-b57a-45b4-94dc-64103cdefca8.pdf",
+  "resource_type": "book",
+  "version": 2
+}
