a47f3c85c61b208edd4202a57046d4c445ad63ee
Your Name <you@example.com>
2024-11-10 23:10:47 +0800

    fix

--- e4b37048-55cf-4030-9a3f-91c0edd8a1d0.ts
+++ e4b37048-55cf-4030-9a3f-91c0edd8a1d0.ts
@@ -1,28 +1,30 @@
 export default {
-  "entity": {
-    "id": "e4b37048-55cf-4030-9a3f-91c0edd8a1d0",
-    "name": "毛泽东的金棍子 姚文元与「文革」（下）",
-    "internal": false,
-    "type": "pdf",
-    "official": false,
-    "author": "约翰·西西弗斯",
-    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives18/main/e4b37048-55cf-4030-9a3f-91c0edd8a1d0.pdf"
+  entity: {
+    id: 'e4b37048-55cf-4030-9a3f-91c0edd8a1d0',
+    name: '毛泽东的金棍子 姚文元与「文革」（下）',
+    internal: false,
+    type: 'pdf',
+    official: false,
+    author: '约翰·西西弗斯',
+    files: [
+      'https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives18/main/e4b37048-55cf-4030-9a3f-91c0edd8a1d0.pdf',
+    ],
   },
-  "parser_option": {
-    "articles": [
+  parser_option: {
+    articles: [
       {
-        "title": "毛泽东的金棍子 姚文元与「文革」（下）",
-        "authors": [
-          "约翰·西西弗斯"
+        title: '毛泽东的金棍子 姚文元与「文革」（下）',
+        authors: [
+          '约翰·西西弗斯',
         ],
-        "page_start": 1,
-        "page_end": 242,
-        "dates": []
-      }
-    ]
+        page_start: 1,
+        page_end: 242,
+        dates: [],
+      },
+    ],
   },
-  "parser_id": "automation",
-  "path": "e4b37048-55cf-4030-9a3f-91c0edd8a1d0.pdf",
-  "resource_type": "book",
-  "version": 2
-}
+  parser_id: 'automation',
+  path: 'e4b37048-55cf-4030-9a3f-91c0edd8a1d0.pdf',
+  resource_type: 'book',
+  version: 2,
+};
